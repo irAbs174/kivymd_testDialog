dd03aab4a0ff6a5e2b906d9618ff17c4098e096b
unique <unique@localhost.localdomain>
2024-08-14 14:13:49 +0330

    load responsive layer

--- app.py
+++ app.py
@@ -1,18 +1,43 @@
 from kivymd.uix.responsivelayout import MDResponsiveLayout
 from kivymd.uix.button import MDButton, MDButtonText
+from kivymd.theming import ThemeManager
 from bidi.algorithm import get_display
 from kivymd.uix.screen import MDScreen
 from kivy.core.text import LabelBase
 from arabic_reshaper import reshape
 from kivy.uix.widget import Widget
 from kivy.lang import Builder
-from kivy.metrics import sp
 from kivymd.app import MDApp
+from kivy.metrics import sp
+
+
+class MobileView(MDScreen):
+    pass
+
+
+class TabletView(MDScreen):
+    pass
+
+
+class DesktopView(MDScreen):
+    pass
+
+
+class ResponsiveView(MDResponsiveLayout, MDScreen):
+    def __init__(self, **kw):
+        super().__init__(**kw)
+        self.mobile_view = MobileView()
+        self.tablet_view = TabletView()
+        self.desktop_view = DesktopView()
 
 
 class UniqueApp(MDApp):
+    title = 'Unique App'
+    screen = None
+
     def build(self):
         self.theme_cls.theme_style = "Dark"
+        self.theme_cls.primary_palette = "Orange"
 
         LabelBase.register(
             name="nasalization",
@@ -33,5 +58,6 @@ class UniqueApp(MDApp):
         reshaped_text = reshape(txt)
         bidi_text = get_display(reshaped_text)
         return bidi_text
+
 
 UniqueApp().run()
