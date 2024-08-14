f035ec6170480b2b2985740687e5e2f337b8d23e
unique <unique@localhost.localdomain>
2024-08-14 16:54:20 +0330

    Bug on nav drawer => no fix git add -Agit add -A

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
+from kivymd.uix.navigationrail import MDNavigationRailItem
 from kivymd.uix.responsivelayout import MDResponsiveLayout
 from kivymd.uix.button import MDButton, MDButtonText
+from kivymd.uix.navigationdrawer import (
+    MDNavigationDrawerItem, MDNavigationDrawerItemTrailingText
+)
+from kivymd.uix.boxlayout import MDBoxLayout
+from kivy.properties import StringProperty, ColorProperty
 from kivymd.theming import ThemeManager
 from bidi.algorithm import get_display
 from kivymd.uix.screen import MDScreen
@@ -9,6 +15,31 @@ from kivy.uix.widget import Widget
 from kivy.lang import Builder
 from kivymd.app import MDApp
 from kivy.metrics import sp
+
+
+class DrawerLabel(MDBoxLayout):
+    icon = StringProperty()
+    text = StringProperty()
+
+
+class DrawerItem(MDNavigationDrawerItem):
+    icon = StringProperty()
+    text = StringProperty()
+    trailing_text = StringProperty()
+    trailing_text_color = ColorProperty()
+
+    _trailing_text_obj = None
+
+    def on_trailing_text(self, instance, value):
+        self._trailing_text_obj = MDNavigationDrawerItemTrailingText(
+            text=value,
+            theme_text_color="Custom",
+            text_color=self.trailing_text_color,
+        )
+        self.add_widget(self._trailing_text_obj)
+
+    def on_trailing_text_color(self, instance, value):
+        self._trailing_text_obj.text_color = value
 
 
 class MobileView(MDScreen):
@@ -31,13 +62,17 @@ class ResponsiveView(MDResponsiveLayout, MDScreen):
         self.desktop_view = DesktopView()
 
 
+class CommonNavigationRailItem(MDNavigationRailItem):
+    text = StringProperty()
+    icon = StringProperty()
+
 class UniqueApp(MDApp):
     title = 'Unique App'
     screen = None
 
     def build(self):
         self.theme_cls.theme_style = "Dark"
-        self.theme_cls.primary_palette = "Orange"
+        self.theme_cls.primary_palette = "Tomato"
 
         LabelBase.register(
             name="nasalization",
